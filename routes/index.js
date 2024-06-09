4906f6db54c6a97e365bc3d0d854dc8f39812eaf
peace <peaces@phreetech.com>
2024-06-09 12:06:00 +0100

    deleted node module

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
 const countries = require("recipiesjs");
 
@@ -15,7 +17,7 @@ const users = [];
 
  //endpoint to get all recipies
  router.get("/", (req, res) => {
-    return res.json(countries);
+    return countries res.json (countries);
  });
 
  //endpoint to get recipie by ID(might change this to name later, but thats more complicated right now)
@@ -29,4 +31,36 @@ const users = [];
         return res.status(404).send('Recipe not found');
     }
 });
- module.exports = router;
+
+
+//Middleware to parse JSON bodies
+router.use(bodyParser.json());
+
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Username and password are required'});
+        }
+
+//Hash the password using bcrypt
+const saltRounds = 10;
+const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+//For demonstration, we're just returning the data.
+res.status(200).json({message: 'User registered Successfully',
+    user: {
+        username,
+        password: hashedPassword
+
+        }
+    }
+)
+
+router.listen(PORT, () => {
+        console.log('Server running at http://localhost:${port}');
+});
+
+    }
+})
