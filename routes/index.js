8b7e9061d59462b459f32acbbbe94dd941fd475e
LAPTOP-A3FDVKS4\HP <chidubemokpalaobieri@gmail.com>
2024-06-09 16:51:01 -0400

    Commited changes 09/06

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const countries = require("recipiesjs");
+const recipe = require("recipe");
 
-const users = [];
 //route for our index page.
 
  router.get("/", (req, res) => {
@@ -13,20 +12,66 @@ const users = [];
      }));
  });
 
- //endpoint to get all recipies
- router.get("/", (req, res) => {
-    return res.json(countries);
- });
 
  //endpoint to get recipie by ID(might change this to name later, but thats more complicated right now)
- //it works based off a data base, but as of 5pm 05/06 i have not implemented this.
+ //it works based off a data base, but as of 09/06 i have not implemented this.
  router.get('/:id', (req, res) => {
-    const recipeId = parseInt(req.params.id);
-    const recipe = recipes.find(r => r.id === recipeId);
-    if (recipe) {
-        return res.json(recipe);
-    } else {
-        return res.status(404).send('Recipe not found');
+    //const recipeId = parseInt(req.params.id);
+    //const recipe = recipe.find(r => r.id === recipeId);
+    //if (recipe) {
+    //    return res.json(recipe);
+    //} else {
+    //    return res.json({error: "Recipe not found"});
+    //}
+});
+
+// Endpoint to create a new recipe
+router.post('/', async (req, res) => {
+    try {
+        const newRecipe = new Recipe(req.body);
+        const recipe = await newRecipe.save();
+        res.status(201).json(recipe);
+    } catch (err) {
+        res.status(400).send('Bad request');
     }
 });
+
+// Endpoint to update a recipe by ID
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (updatedRecipe) {
+            res.json(updatedRecipe);
+        } else {
+            res.status(404).send('Recipe not found');
+        }
+    } catch (err) {
+        res.status(400).send('Bad request');
+    }
+});
+
+// Endpoint to delete a recipe by ID
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id);
+        if (deletedRecipe) {
+            res.json(deletedRecipe);
+        } else {
+            res.status(404).send('Recipe not found');
+        }
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
+// Endpoint to fetch popular recipes
+router.get('/popular', async (req, res) => {
+    try {
+        const popularRecipes = await Recipe.find().sort({ popularity: -1 }).limit(5); // Fetch top 5 popular recipes
+        res.json(popularRecipes);
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
  module.exports = router;
