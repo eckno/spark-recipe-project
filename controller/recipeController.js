b287d9404ed7cc72f733ce49e01dbfcfc97e947c
Chizaram16 <peaces@phreetech.com>
2024-06-23 17:48:06 +0100

    peace commit

--- controller/recipeController.js
+++ controller/recipeController.js
@@ -54,8 +54,154 @@ class RecipeController {
             return res.status(400).send(response);
         }
     }
-};
+
+
+    async postDeleteRecipe(req, res) {
+        const response = {};
+        try{
+            const post = req.body || {};
+            //
+            if(post){
+                const title = post.title || "";
+                const ingredients = post.ingredients || "";
+                const instructions = post.instructions || "";
+            
+                if (!title.trim() || !ingredients.trim() || !instructions.trim() ){
+                    response['success'] = false;
+                    response['error_message'] = "You have successfully deleted your recipe!";
+
+                    return res.status(400).send(response);
+                }
+                
+
+                //NOW LETS SUBMIT TO DB
+                const data_to_submit = {
+                    title: title,
+                    ingredients: ingredients,
+                    instructions: instructions,
+                }
+
+                const db_connect = await DBConnect();
+                const collection = db_connect.db().collection(this.collectionName);
+
+                const add_new = await collection.insertOne(data_to_submit);
+                if(add_new){
+                    response['success'] = true;
+                    response['data'] = add_new;
+
+                    return res.status(201).send(response);
+                }
+
+                response['success'] = false;
+                response['error_message'] = "Oops! we have encountered an issue, please try again later or contact support";
+                return res.status(400).send(response);
+            }
+        } catch (e) {
+            console.log(e);
+            response['success'] = false;
+            response['error_message'] = "Oops! we have encountered an issue, please try again later or contact support";
+            return res.status(400).send(response);
+        }
+    }
+
+
+    async postEditRecipe(req, res) {
+        const response = {};
+        try{
+            const post = req.body || {};
+            //
+            if(post){
+                const title = post.title || "";
+                const ingredients = post.ingredients || "";
+                const instructions = post.instructions || "";
+            
+                if (!title.trim() || !ingredients.trim() || !instructions.trim() ){
+                    response['success'] = false;
+                    response['error_message'] = "Kindly make sure all required fields are correctly filled";
+
+                    return res.status(400).send(response);
+                }
+                
+
+                //NOW LETS SUBMIT TO DB
+                const data_to_submit = {
+                    title: title,
+                    ingredients: ingredients,
+                    instructions: instructions,
+                }
+
+                const db_connect = await DBConnect();
+                const collection = db_connect.db().collection(this.collectionName);
+
+                const add_new = await collection.insertOne(data_to_submit);
+                if(add_new){
+                    response['success'] = true;
+                    response['data'] = add_new;
+
+                    return res.status(201).send(response);
+                }
+
+                response['success'] = false;
+                response['error_message'] = "Oops! we have encountered an issue, please try again later or contact support";
+                return res.status(400).send(response);
+            }
+        } catch (e) {
+            console.log(e);
+            response['success'] = false;
+            response['error_message'] = "Oops! we have encountered an issue, please try again later or contact support";
+            return res.status(400).send(response);
+        }
+    }
+
+
+    async getRecipe(req, res) {
+        const response = {};
+        try{
+            const get = req.body || {};
+            //
+            if(post){
+                const title = post.title || "";
+                const ingredients = post.ingredients || "";
+                const instructions = post.instructions || "";
+            
+                if (!title.trim() || !ingredients.trim() || !instructions.trim() ){
+                    response['success'] = false;
+                    response['error_message'] = "Kindly make sure all required fields are correctly filled";
+
+                    return res.status(400).send(response);
+                }
+                
+
+                //NOW LETS SUBMIT TO DB
+                const data_to_submit = {
+                    title: title,
+                    ingredients: ingredients,
+                    instructions: instructions,
+                }
+
+                const db_connect = await DBConnect();
+                const collection = db_connect.db().collection(this.collectionName);
+
+                const add_new = await collection.insertOne(data_to_submit);
+                if(add_new){
+                    response['success'] = true;
+                    response['data'] = add_new;
+
+                    return res.status(201).send(response);
+                }
+
+                response['success'] = false;
+                response['error_message'] = "Oops! we have encountered an issue, please try again later or contact support";
+                return res.status(400).send(response);
+            }
+        } catch (e) {
+            console.log(e);
+            response['success'] = false;
+            response['error_message'] = "Oops! we have encountered an issue, please try again later or contact support";
+            return res.status(400).send(response);
+        }
+    }
 
 
 
-module.exports = RecipeController;
+    module.exports = RecipeController;
--- routes/index.js
+++ routes/index.js
@@ -3,57 +3,6 @@ const router = express.Router();
 const UserController = require("../controller/userController");
 const RecipeController = require("../controller/recipeController");
 
-<<<<<<< HEAD
-=======
-
- router.get("/", (req, res) => {
-     res.render("index", (req, {
-         title: "Index Page"
-     }));
- });
- 
- router.get('/:id', (req, res) => {
-    //no logic yet
-});
-
-router.post('/', async (req, res) => {
-    try {
-        const newRecipe = new Recipe(req.body);
-        const recipe = await newRecipe.save();
-        res.status(201).json(recipe);
-    } catch (err) {
-        res.status(400).send('Bad request');
-    }
-});
-
-// Endpoint to update a recipe by name
- router.put('/:update', async (req, res) => {
-     const userController = new UserController();
-     return userController.updateRecipe(req, res);
- });
-
-// Endpoint to delete a recipe by ID
-router.delete('/:delete', async (req, res) => {
-    const userController = new UserController();
-    return userController.deleteRecipe(req, res);
-});
-
-// Endpoint to fetch popular recipes
-/*router.get('/popular', async (req, res) => {
-    try {
-        const popularRecipes = await Recipe.find().sort({ popularity: -1 }).limit(5); // Fetch top 5 popular recipes
-        res.json(popularRecipes);
-    } catch (err) {
-        res.status(500).send('Server error');
-    }
-});*/
-
-router.get('/search/:key', async (req, res) => {
-   const userController = new UserController();
-   return userController.recipeSearch(req, res);
-    }
-);
->>>>>>> b63d2bab479f259a61591d7c98c0c36045f8bd22
 
 router.post('/add_new_user', async (req, res) => {
    const userController = new UserController();
@@ -65,33 +14,21 @@ router.post('/addrecipe', async (req, res) => {
   return recipeController.postAddNewRecipe(req, res);
 });
 
-//router.post('/add_recipe', async (req, res) => {
-  //  const  userController = new UserController();
-    //return userController.postaddRecipe(req, res);
 
-//});
+router.delete('/deleterecipe', async (req, res) => {
+const  recipeController = new RecipeController();
+return recipeController.postDeleteRecipe(req, res);
+});
 
-//router.delete('/delete_recipe', async (req, res) => {
-  //  const  userController = new UserController();
-    //return userController.postdeleteRecipe(req, res);
-//});
+router.post('/editrecipe', async (req, res) => {
+const  recipeController = new RecipeController();
+return recipeController.postEditRecipe(req, res);
+});
 
-//router.put('/edit_recipe', async (req, res) => {
-  //  const  userController = new UserController();
-    //return userController.puteditRecipe(req, res);
-//});
-
-//router.get('/recipe', async (req, res) => {
-  //  const  userController = new UserController();
-    //return userController.getRecipe(req, res);
-//});
-
-//Routes for recipe operations
-//router.post('/add-recipe', userController.addRecipe);
-//router.delete('/delete-recipe/:recipeId', userController.deleteRecipe);
-//router.put('/edit-recipe/:recipeId', userController.editRecipe);
-//router.get('/recipe/:recipeId', userController.getRecipe);
-
+router.get('/recipe', async (req, res) => {
+const  recipeController = new RecipeController();
+return recipeController.getRecipe(req, res);
+});
 
 
 module.exports = router;
