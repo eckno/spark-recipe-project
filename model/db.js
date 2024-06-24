48ff7c48d56eb22d6ad37b48485f4aa2539185ce
Kennetha <kennetha@phreetech.com>
2024-06-24 13:26:18 +0100

    online db url

--- model/db.js
+++ model/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
+const uri = process.env.MONGODB_URI || 'mongodb+srv://spark_intern:14o37mniiVtPnu9V@spark-intern.u6obgpa.mongodb.net/?retryWrites=true&w=majority&appName=spark-intern';
 let client;
 
 async function connectToDatabase() {
